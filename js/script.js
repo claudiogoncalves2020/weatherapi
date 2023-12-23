a60c95b79a6ddf926baac106fde764b997d38bbe
Claudio Goncalves <claudiojmg1@hotmail.de>
2023-12-23 22:19:09 +0100

    add style css

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const apiKey = "b64c250e08573b3f5a32e73a2e55866b";
+const apiCoutryURL = "https://flagsapi.com/flat/64.png";
+
+const cityinput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+searchBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("test");
+});
